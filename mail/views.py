6f66d4ffab8e6e823694614b2f2386e1bffe4042
Boop-Py <boopthecode@outlook.com>
2020-08-19 15:11:15 +0100

    about to submit

--- mail/views.py
+++ mail/views.py
@@ -177,3 +177,6 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "mail/register.html")
+
+def todolist(request):
+    return render(request, "mail/specifications.html")
